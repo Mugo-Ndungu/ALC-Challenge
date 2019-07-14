43d51bb40630f2857dc45ba10d88437dcfe4ffad
Mugo-Ndungu <twinnymugo@gmail.com>
2019-07-14 22:55:05 +0300

    Add pointer to toolbar

--- app/src/main/java/com/example/alcchall/MainActivity.java
+++ app/src/main/java/com/example/alcchall/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
 
 
